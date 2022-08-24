333a70c8468996e098fa316e1b4298d1c5c83182
Oleg Vasilenok <431502+OlegFlores@users.noreply.github.com>
2022-08-24 02:07:11 +0200

    feat: refactor, add some info to output

--- examples/elf64-parse/src/app.c
+++ examples/elf64-parse/src/app.c
@@ -4,12 +4,8 @@
 #include <glib.h>
 #include "app.h"
 
-GHashTable* _machines;
+const char MAGIC_ELF[MAGIC_ELF_LENGTH] = { 0x7f, 'E', 'L', 'F' };
 
-// /bin/true:     file format elf64-x86-64
-// architecture: i386:x86-64, flags 0x00000150:
-// HAS_SYMS, DYNAMIC, D_PAGED
-// start address 0x0000000000002610
 /**
  * 
   ELF Header:
@@ -43,8 +39,8 @@ void print_elf_info(elf64_info* info) {
     printf("Version:\t\t\t%s\n", info -> version);
     printf("OS/ABI:\t\t\t\t%s\n", info -> os_abi);
     printf("ABI Version:\t\t\t%s\n", info -> abi_version);
-
-    printf("architecture: %s, flags: %s:\n", info -> arch, info -> flags_hex);
+    printf("Padding:\t\t\t%s\n", info -> padding_bytes);
+    printf("Type:\t\t\t\t%s\n", info -> object_file_type);
 }
 
 int args_have_errors(int argc, char **argv) {
@@ -53,102 +49,6 @@ int args_have_errors(int argc, char **argv) {
     } else {
         return 0;
     }
-}
-
-char* find_machine_description(GHashTable* machines, int machine_value) {
-    void* res = g_hash_table_lookup(machines, GINT_TO_POINTER(machine_value));
-    if(res == NULL) {
-        return "N/A";
-    } else {
-        return (char*)res;
-    }
-} 
-
-void create_hashtable_machines(GHashTable* machines) {
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x00 ), strdup("No specific instruction set") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x01 ), strdup("AT&T WE 32100") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x02 ), strdup("SPARC") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x03 ), strdup("x86") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x04 ), strdup("Motorola 68000 (M68k)") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x05 ), strdup("Motorola 88000 (M88k)") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x06 ), strdup("Intel MCU") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x07 ), strdup("Intel 80860") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x08 ), strdup("MIPS") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x09 ), strdup("IBM System/370") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x0A ), strdup("MIPS RS3000 Little-endian") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x0B ), strdup("Reserved for future use") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x0C ), strdup("Reserved for future use") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x0D ), strdup("Reserved for future use") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x0E ), strdup("Hewlett-Packard PA-RISC") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x0F ), strdup("Reserved for future use") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x13 ), strdup("Intel 80960") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x14 ), strdup("PowerPC") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x15 ), strdup("PowerPC (64-bit)") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x16 ), strdup("S390, including S390x") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x17 ), strdup("IBM SPU/SPC") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x18 ), strdup("Reserved for future use") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x19 ), strdup("Reserved for future use") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x1A ), strdup("Reserved for future use") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x1B ), strdup("Reserved for future use") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x1C ), strdup("Reserved for future use") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x1D ), strdup("Reserved for future use") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x1E ), strdup("Reserved for future use") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x1F ), strdup("Reserved for future use") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x20 ), strdup("Reserved for future use") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x21 ), strdup("Reserved for future use") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x22 ), strdup("Reserved for future use") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x23 ), strdup("Reserved for future use") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x24 ), strdup("NEC V800") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x25 ), strdup("Fujitsu FR20") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x26 ), strdup("TRW RH-32") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x27 ), strdup("Motorola RCE") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x28 ), strdup("ARM (up to ARMv7/Aarch32)") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x29 ), strdup("Digital Alpha") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x2A ), strdup("SuperH") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x2B ), strdup("SPARC Version 9") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x2C ), strdup("Siemens TriCore embedded processor") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x2D ), strdup("Argonaut RISC Core") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x2E ), strdup("Hitachi H8/300") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x2F ), strdup("Hitachi H8/300H") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x30 ), strdup("Hitachi H8S") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x31 ), strdup("Hitachi H8/500") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x32 ), strdup("IA-64") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x33 ), strdup("Stanford MIPS-X") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x34 ), strdup("Motorola ColdFire") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x35 ), strdup("Motorola M68HC12") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x36 ), strdup("Fujitsu MMA Multimedia Accelerator") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x37 ), strdup("Siemens PCP") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x38 ), strdup("Sony nCPU embedded RISC processor") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x39 ), strdup("Denso NDR1 microprocessor") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x3A ), strdup("Motorola Star*Core processor") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x3B ), strdup("Toyota ME16 processor") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x3C ), strdup("STMicroelectronics ST100 processor") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x3D ), strdup("Advanced Logic Corp. TinyJ embedded processor family") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x3E ), strdup("AMD x86-64") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x3F ), strdup("Sony DSP Processor") );
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x40 ), strdup("Digital Equipment Corp. PDP-10") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x41 ), strdup("Digital Equipment Corp. PDP-11") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x42 ), strdup("Siemens FX66 microcontroller") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x43 ), strdup("STMicroelectronics ST9+ 8/16 bit microcontroller") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x44 ), strdup("STMicroelectronics ST7 8-bit microcontroller") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x45 ), strdup("Motorola MC68HC16 Microcontroller") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x46 ), strdup("Motorola MC68HC11 Microcontroller") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x47 ), strdup("Motorola MC68HC08 Microcontroller") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x48 ), strdup("Motorola MC68HC05 Microcontroller") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x49 ), strdup("Silicon Graphics SVx") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x4A ), strdup("STMicroelectronics ST19 8-bit microcontroller") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x4B ), strdup("Digital VAX") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x4C ), strdup("Axis Communications 32-bit embedded processor") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x4D ), strdup("Infineon Technologies 32-bit embedded processor") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x4E ), strdup("Element 14 64-bit DSP Processor") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x4F ), strdup("LSI Logic 16-bit DSP Processor") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x8C ), strdup("TMS320C6000 Family") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0xAF ), strdup("MCST Elbrus e2k") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0xB7 ), strdup("ARM 64-bits (ARMv8/Aarch64)") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0xDC ), strdup("Zilog Z80") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0xF3 ), strdup("RISC-V") ); 
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0xF7 ), strdup("Berkeley Packet Filter") );
-    g_hash_table_insert( machines, GINT_TO_POINTER( 0x101 ), strdup("WDC 65C816") );
 }
 
 int read_elf_header(FILE* fd, Elf64_Ehdr* elf_header) {
@@ -160,6 +60,81 @@ int read_elf_header(FILE* fd, Elf64_Ehdr* elf_header) {
     }
     return 0;    
 }
+
+char* map_osabi_desc(unsigned char osabi_id) {
+    switch(osabi_id) {
+        case 0x00: return "UNIX - System V";
+        case 0x01: return "HP-UX";
+        case 0x02: return "NetBSD";
+        case 0x03: return "Linux";
+        case 0x04: return "GNU Hurd";
+        case 0x06: return "Solaris";
+        case 0x07: return "AIX (Monterey)";
+        case 0x08: return "IRIX";
+        case 0x09: return "FreeBSD";
+        case 0x0A: return "Tru64";
+        case 0x0B: return "Novell Modesto";
+        case 0x0C: return "OpenBSD";
+        case 0x0D: return "OpenVMS";
+        case 0x0E: return "NonStop Kernel";
+        case 0x0F: return "AROS";
+        case 0x10: return "FenixOS";
+        case 0x11: return "Nuxi CloudABI";
+        case 0x12: return "Stratus Technologies OpenVOS";
+    }
+    return "N/A";
+}
+
+char* map_object_file_type_desc(Elf64_Half object_file_type_id) {
+    switch(object_file_type_id) {
+        case ET_NONE: return "NONE (Unknown)";
+        case ET_REL: return "REL (Relocatable file)";
+        case ET_EXEC: return "EXEC (Executable file)";
+        case ET_DYN: return "DYN (Position-Independent Executable file)";
+        case ET_CORE: return "Core (Core file)";
+        default: return "Reserved inclusive range";
+    }
+    return "N/A";
+}
+
+char* map_class_desc(unsigned char class_id) {
+    switch(class_id) {
+        case 1:
+            return "ELF32";
+        case 2:
+            return "ELF64";
+    }
+    return "N/A";
+}
+
+char* map_data_desc(unsigned char data_id) {
+    switch(data_id) {
+        case 1:
+            return "2's complement, little endian";
+        case 2:
+            return "2's complement, big endian";
+    }
+    return "N/A";
+}
+
+char* map_version_desc(unsigned char version_id) {
+    switch(version_id) {
+        case 1:
+            return "1 (current)";
+    }
+    return "N/A";
+}
+
+char* map_raw_bytes_desc(unsigned char* magic_bytes_arr, size_t n_bytes, char* out_str) {
+    for(int i = 0; i < n_bytes; i++) {
+        snprintf(&out_str[i*3], 3, "%02x", magic_bytes_arr[i]);
+        if(i < n_bytes - 1) {
+            snprintf(&out_str[i*3 + 2], 2, " ");
+        }
+    }
+    return out_str;
+}
+
 
 void print_key_value( gpointer key, gpointer value, gpointer user_data ) {
     int real_key = GPOINTER_TO_INT( key );
@@ -174,6 +149,10 @@ int run_app(int argc, char **argv) {
     Elf64_Ehdr elf_header;
     elf64_info file_info; 
     FILE* fd;
+    char flags[19];
+    char magic_bytes[48];
+    char padding_bytes[21];
+    char abi_version[4];
     
     if(args_have_errors(argc, argv) != 0) {
         printf(CLI_MESSAGE_USAGE_HINT);
@@ -182,89 +161,35 @@ int run_app(int argc, char **argv) {
     
     file_name = argv[1];
     fd = fopen(file_name, "rb"); 
+
+    if(fd == NULL) {
+        printf(CLI_MESSAGE_COULD_NOT_OPEN_FILE);
+        return ERROR_FILE_CANNOT_BE_OPENED;
+    }
     
     if(read_elf_header(fd, &elf_header) != 0) {
         printf(CLI_MESSAGE_FORMAT_NOT_RECOGNIZED, file_name);
         return ERROR_FILE_NOT_ELF;
     }
 
-    char flags[19];
+    snprintf(abi_version, 4, "%d", elf_header.e_ident[EI_ABIVERSION]);
     snprintf( flags, 19, "0x%016x",  elf_header.e_flags);
-    char magic_bytes[48];
-    for(int i = 0; i < 16; i++) {
-        snprintf(&magic_bytes[i*3], 3, "%02x", elf_header.e_ident[i]);
-        if(i < 15) {
-            snprintf(&magic_bytes[i*3 + 2], 2, " ");
-        }
-    }
-    
-
-    _machines = g_hash_table_new_full(g_direct_hash, g_direct_equal, NULL, g_free);
-    create_hashtable_machines(_machines);
 
     file_info.file_name = file_name;
-    file_info.magic_bytes = magic_bytes;
-    switch(elf_header.e_ident[EI_CLASS]) {
-        case 1:
-            file_info.class = "ELF32";
-            break;
-        case 2:
-            file_info.class = "ELF64";
-            break;
-        default:
-            file_info.class = "N/A";
-    }
-    switch(elf_header.e_ident[EI_DATA]) {
-        case 1:
-            file_info.endiannes = "2's complement, little endian";
-            break;
-        case 2:
-            file_info.endiannes = "2's complement, big endian";
-            break;
-        default:
-            file_info.class = "N/A";
-    }
-    switch(elf_header.e_ident[EI_VERSION]) {
-        case 1:
-            file_info.version = "1 (current)";
-            break;
-        default:
-            file_info.version = "N/A";
-    }
-    switch(elf_header.e_ident[EI_OSABI]) {
-        case 0x00: file_info.os_abi = "UNIX - System V"; break;
-        case 0x01: file_info.os_abi = "HP-UX"; break;
-        case 0x02: file_info.os_abi = "NetBSD"; break;
-        case 0x03: file_info.os_abi = "Linux"; break;
-        case 0x04: file_info.os_abi = "GNU Hurd"; break;
-        case 0x06: file_info.os_abi = "Solaris"; break;
-        case 0x07: file_info.os_abi = "AIX (Monterey)"; break;
-        case 0x08: file_info.os_abi = "IRIX"; break;
-        case 0x09: file_info.os_abi = "FreeBSD"; break;
-        case 0x0A: file_info.os_abi = "Tru64"; break;
-        case 0x0B: file_info.os_abi = "Novell Modesto"; break;
-        case 0x0C: file_info.os_abi = "OpenBSD"; break;
-        case 0x0D: file_info.os_abi = "OpenVMS"; break;
-        case 0x0E: file_info.os_abi = "NonStop Kernel"; break;
-        case 0x0F: file_info.os_abi = "AROS"; break;
-        case 0x10: file_info.os_abi = "FenixOS"; break;
-        case 0x11: file_info.os_abi = "Nuxi CloudABI"; break;
-        case 0x12: file_info.os_abi = "Stratus Technologies OpenVOS"; break;
-        default: file_info.os_abi = "N/A";
-    }
-    char abi_version[4];
-    snprintf(abi_version, 4, "%d", elf_header.e_ident[EI_ABIVERSION]);
+    file_info.magic_bytes = map_raw_bytes_desc(elf_header.e_ident, 16, magic_bytes);;
+    file_info.class = map_class_desc(elf_header.e_ident[EI_CLASS]);
+    file_info.endiannes = map_data_desc(elf_header.e_ident[EI_DATA]);
+    file_info.version = map_version_desc(elf_header.e_ident[EI_VERSION]);
+    file_info.os_abi = map_osabi_desc(elf_header.e_ident[EI_OSABI]);
     file_info.abi_version = abi_version;
-    
-    file_info.arch = find_machine_description(_machines, elf_header.e_machine);
+    file_info.padding_bytes = map_raw_bytes_desc(&elf_header.e_ident[EI_PAD], 7, padding_bytes);
+    file_info.object_file_type = map_object_file_type_desc(elf_header.e_type);
     file_info.flags_hex = flags;
 
     print_elf_info(&file_info);
-    // CLEAN-UP:
-    g_hash_table_destroy( _machines );
-    _machines = NULL;
-
+    
     fclose(fd);
-    return 0; 
-    // Print out the contents of the map: g_hash_table_foreach( _machines, print_key_value, NULL );
+    return 0;
 }
+
+
--- examples/elf64-parse/src/app.h
+++ examples/elf64-parse/src/app.h
@@ -1,29 +1,43 @@
 
+#ifndef _APP_H
+
 #define MAGIC_ELF_LENGTH 4
 
 #define ERROR_WRONG_ARGUMENTS 10
 #define ERROR_FILE_NOT_ELF 20
+#define ERROR_FILE_CANNOT_BE_OPENED 30
 
 #define CLI_MESSAGE_USAGE_HINT "Run: `elf64-parse _elf64_file_path_`\n"
 #define CLI_MESSAGE_FORMAT_NOT_RECOGNIZED "elf64-parse: %s: file format not recognized\n"
+#define CLI_MESSAGE_COULD_NOT_OPEN_FILE "Could not open file\n"
 #define CLI_MESSAGE_FORMAT_ELF32 "elf64-x86-32"
 #define CLI_MESSAGE_FORMAT_ELF64 "elf64-x86-64"
 
-const char MAGIC_ELF[MAGIC_ELF_LENGTH] = { 0x7f, 'E', 'L', 'F' };
+extern const char MAGIC_ELF[MAGIC_ELF_LENGTH];
 
 typedef struct elf64_info_struct {
     char* file_name;
-    char* magic_bytes; // space delimeted 16 first bytes
+    char* magic_bytes; // space delimited 16 first bytes
     char* class; // ELF64
     char* endiannes; // 2's complement, little endian
     char* version; // 1 (current)
     char* os_abi; // UNIX - System V
     char* abi_version; // 0
+    char* padding_bytes; // space delimited 7 bytes that should all be 00
+    char* object_file_type; // DYN (Position-Independent Executable file)
+
     char* arch;
     char* flags_hex;
     char* link_info;
     char* start_addr_hex;
 } elf64_info;
 
+typedef union
+{
+    unsigned int integer;
+    unsigned char bytes[8];
+} int_per_bytes;
+
 int run_app(int argc, char **argv);
 
+#endif
